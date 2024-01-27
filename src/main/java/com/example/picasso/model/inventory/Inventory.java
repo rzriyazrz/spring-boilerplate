4365b9a9d5f667d9f4f46f6a0bced46bc10b1f6e
Riyaz Mohammed <rzriyazrz@gmail.com>
2024-01-27 13:56:11 +0530

    feat:created inventory model

--- src/main/java/com/example/picasso/model/inventory/Inventory.java
+++ src/main/java/com/example/picasso/model/inventory/Inventory.java
@@ -6,17 +6,33 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @Getter
 @Setter
+
 @Document(collection = "inventory")
 public class Inventory {
 
     @Id
     private String id;
+
     private String title;
     private String desc;
+    private String name;
+    private double price;
+    private int currentStock;
+    private int itemsSold;
+    private float discount;
+    private String category;
+    private String brand;
 
 
-    public Inventory(String title, String desc) {
+    public Inventory(String title, String desc, String name, double price, int currentStock, int itemsSold, float discount, String category,String brand) {
         this.title = title;
         this.desc = desc;
+        this.name = name;
+        this.price = price;
+        this.currentStock = currentStock;
+        this.itemsSold = itemsSold;
+        this.discount = discount;
+        this.category = category;
+        this.brand = brand;
     }
 }
