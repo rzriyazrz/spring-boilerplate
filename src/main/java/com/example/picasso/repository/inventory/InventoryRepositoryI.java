97a22b5a151662fe63345c85b100f561d171f01a
Riyaz Mohammed <rzriyazrz@gmail.com>
2024-01-27 13:58:08 +0530

    feat: added findByFilters custom query in InventoryRepository

--- src/main/java/com/example/picasso/repository/inventory/InventoryRepositoryI.java
+++ src/main/java/com/example/picasso/repository/inventory/InventoryRepositoryI.java
@@ -2,9 +2,18 @@ package com.example.picasso.repository.inventory;
 
 import com.example.picasso.model.inventory.Inventory;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Repository
 public interface InventoryRepositoryI extends MongoRepository<Inventory,String> {
+    @Query("{'title': { $regex: /?0/ }, 'category': { $in: ?1 }, 'brand': { $in: ?2 } }")
+    List<Inventory> findByfilters(String search,List<String> categories,List<String> brands);
+
+
+
+
 }
