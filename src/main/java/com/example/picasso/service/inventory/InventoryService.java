6e066136450d8aa5d7b4713bd2bb2772bd07354b
Riyaz Mohammed <rzriyazrz@gmail.com>
2024-01-27 14:08:46 +0530

    feat: added function getInventories to filter the inventories,removed searchInventory function,modified the saveInventory method to recieve additonal parameters

--- src/main/java/com/example/picasso/service/inventory/InventoryService.java
+++ src/main/java/com/example/picasso/service/inventory/InventoryService.java
@@ -1,9 +1,14 @@
 package com.example.picasso.service.inventory;
 
+import com.example.picasso.model.inventory.Filter;
 import com.example.picasso.model.inventory.Inventory;
 import com.example.picasso.repository.inventory.InventoryRepositoryI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Service
 public class InventoryService {
@@ -11,15 +16,29 @@ public class InventoryService {
     @Autowired
     InventoryRepositoryI inventoryRepositoryI;
 
-    public void saveInventory(String title,String desc){
-        Inventory inventory = new Inventory(title, desc);
+    public void saveInventory(String title, String desc, String name, double price, int currentStock, int itemsSold, float discount, String category, String brand) {
+        Inventory inventory = new Inventory(title, desc, name, price, currentStock, itemsSold, discount, category, brand);
         inventoryRepositoryI.save(inventory);
     }
-    public void searchInventory(){
-        //TODO
-    }
-    public void deleteInventory(String id){
+
+    public void deleteInventory(String id) {
         inventoryRepositoryI.deleteById(id);
+    }
+
+    public List<Inventory> getInventories(String search, List<Filter> filters, int pageSize, String sort, String sortKey, int page) {
+        List<String> categories = new ArrayList<>();
+        List<String> brands = new ArrayList<>();
+
+        for (Filter filter : filters) {
+            if (filter.getKey().equals("category"))
+                categories = filter.getValues();
+            else if (filter.getKey().equals("brand"))
+                brands = filter.getValues();
+
+        }
+
+        List<Inventory> inventories = inventoryRepositoryI.findByfilters(search, categories, brands);
+        return inventories;
     }
 
 }
