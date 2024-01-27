4f969ca7e05101b14912c0d31ffc0e3c9505f21b
Riyaz Mohammed <rzriyazrz@gmail.com>
2024-01-27 13:55:31 +0530

    feat:created inventorycreaterequestDTO

--- src/main/java/com/example/picasso/dto/inventory/InventoryCreateRequestDTO.java
+++ src/main/java/com/example/picasso/dto/inventory/InventoryCreateRequestDTO.java
@@ -5,15 +5,71 @@ public class InventoryCreateRequestDTO {
     private String title;
     private String desc;
 
-    public InventoryCreateRequestDTO(String title, String desc) {
+    private String name;
+    private double price;
+    private int currentStock;
+    private int itemsSold;
+    private float discount;
+    private String category;
+
+    private String brand;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getCurrentStock() {
+        return currentStock;
+    }
+
+    public int getItemsSold() {
+        return itemsSold;
+    }
+
+    public float getDiscount() {
+        return discount;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public InventoryCreateRequestDTO(String title, String desc, String name, double price, int currentStock, int itemsSold, float discount, String category) {
         this.title = title;
         this.desc = desc;
+        this.name = name;
+        this.price = price;
+        this.currentStock = currentStock;
+        this.itemsSold = itemsSold;
+        this.discount = discount;
+        this.category = category;
     }
-    public String getTitle() {
-        return this.title;
-    }
-    public String getDesc() {
-        return this.desc;
-    }
+
+//    public InventoryCreateRequestDTO(String title, String desc) {
+//        this.title = title;
+//        this.desc = desc;
+//    }
+//    public String getTitle() {
+//        return this.title;
+//    }
+//    public String getDesc() {
+//        return this.desc;
+//    }
 
 }
